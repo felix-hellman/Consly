109e1296e4d771aa37c9e5d094b3befb42f67a2e
Adam Georgsson <adamgeorgsson@gmail.com>
2017-11-24 16:31:46 +0100

    Change so it accepts any GET or POST parameter

--- test/webtest.php
+++ test/webtest.php
@@ -1,18 +1,13 @@
 <?php
-$json = json_encode(
-    array(
-        'GET' => array(
-            'Title' => isset($_GET['Title']) ? $_GET['Title'] : null,
-            'Title2' => isset($_GET['Title2']) ? $_GET['Title2'] : null,
-            'Data' => isset($_GET['Data']) ? $_GET['Data'] : null,
-            'Data2' => isset($_GET['Data2']) ? $_GET['Data2'] : null
-        ),
-        'POST' => array('Title' => isset($_POST['Title']) ? $_GET['Title'] : null,
-            'Title2' => isset($_POST['Title2']) ? $_GET['Title2'] : null,
-            'Data' => isset($_POST['Data']) ? $_POST['Data'] : null,
-            'Data2' => isset($_POST['Data2']) ? $_POST['Data2'] : null
-        )
-    )
-);
+if (!empty($_GET)) {
+    $requests['GET'] = $_GET;
+} elseif (!empty($_POST)) {
+    $requests['POST'] = $_POST;
+}
 
-print_r(json_decode($json));
+if (isset($requests)) {
+    $json = json_encode($requests, JSON_PRETTY_PRINT);
+    echo "$json\n";
+} else {
+    echo "No GET or POST parameter given\n";
+}
